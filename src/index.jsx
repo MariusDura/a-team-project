a1b346e0a6f15b2e2537268ba00e09b07820c51f
MariusDura <termosroviukas@gmail.com>
2017-03-07 09:48:05 +0200

    Created project

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+require('./../res/styles/index.css');
+
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('app')
+);
