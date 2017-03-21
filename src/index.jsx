587dc61c4c5ce2b9a3d6ba21e5b18fa58fb4cca1
RositaCet <rrnuge@gmail.com>
2017-03-21 09:49:11 +0200

    Revert "Revert "Fixed no scrolling""
    This reverts commit bcd8ebe5b99b5c601e35a8ce65e3c36fd210c74c.

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
-import HelloMoskwa from './components/Hello';
 import Buttonas from './components/Buttonas';
 import Login from './components/Login';
 import LoginLayout from './components/LoginLayout';
 import ForgotPassword from './components/ForgotPassword';
 import Calendar from './components/Calendar';
 import Register from './components/Register';
-import Testas from './components/Testas';
 
 
 require('./../res/styles/index.css');
