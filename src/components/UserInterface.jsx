23258d9bb99398889573547ca75d1e262dd1fd8a
MariusDura <termosroviukas@gmail.com>
2017-03-30 12:52:32 +0300

    Accidentaly removed few lines from Haris code due to GitHub problems

--- src/components/UserInterface.jsx
+++ src/components/UserInterface.jsx
@@ -4,6 +4,9 @@ export default class UserInterface extends Component {
 render() {
     return(
         <div class="btn-group-vertical">
+            <button type="button" className="btn btn-primary">Your Profile</button>
+            <button type="button" className="btn btn-primary">Calendar</button>
+            <button type="button" className="btn btn-primary">Add reminder</button>
         </div>
         );
     }
